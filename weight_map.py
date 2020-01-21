2e1864ccce6885a14c5d9ce2d92e8864abf7b74a
zhanglf-tmac <60082189+zhanglf-tmac@users.noreply.github.com>
2020-01-21 16:57:55 +0800

    Update weight_map.py

--- weight_map.py
+++ weight_map.py
@@ -1,41 +1,41 @@
 
-#【！】代码解读请参考：https://www.jianshu.com/p/60ce8b93b806
-#【！】权重图用于 Unet 和 InvUnet 的隐式轮廓学习
+#【！】please refer to https://www.jianshu.com/p/60ce8b93b806
+#【！】weight map is used for implict contour learning of Unet-w and InvUnet-w
 
 from skimage import io
 import matplotlib.pyplot as plt
 from skimage import measure,color
 import cv2
 
-# 【0】 加载测试图片
+# 【0】 load test image
 gt = io.imread('bin_img.bmp')
 gt = 1 * (gt >0)
 plt.figure(figsize = (10,10))
 plt.imshow(gt)
 plt.show()
 
-# 【1】计算细胞和背景的像素频率
+# 【1】Calculate pixel frequency of cells and background
 c_weights = np.zeros(2)
 c_weights[0] = 1.0 / ((gt == 0).sum())
 c_weights[1] = 1.0 / ((gt == 1).sum())
 
-# 【2】归一化
-c_weights /= c_weights.min() #除以最小数而非最大数
+# 【2】normalization
+c_weights /= c_weights.min() 
 
-# 【3】得到 class_weight map(cw_map)
+# 【3】get class_weight map(cw_map)
 cw_map = np.where(gt==0, c_weights[0], c_weights[1])
 plt.figure(figsize = (10,10))
 plt.imshow(cw_map)
 plt.show()
 
-#【4】连通域分析，彩色化
+#【4】connected component analysis and colorization
 cells = measure.label(gt, connectivity=2)
 cells_color = color.label2rgb(cells , bg_label = 0,  bg_color = (0, 0, 0)) 
 plt.figure(figsize = (10,10))
 plt.imshow(cells_color)
 plt.show()
 
-#【5】计算得到 distance weight map (dw_map)
+#【5】calculate distance weight map (dw_map)
 w0 = 10
 sigma = 5
 dw_map = np.zeros_like(gt)
@@ -52,7 +52,7 @@ plt.figure(figsize = (10,10))
 plt.imshow(dw_map, cmap = 'jet')
 plt.show()
 
-# 【6】 合并 cw_map 和 dw_map 得到最终的 weight map
+# 【6】 add cw_map and dw_map, and get the final weight map
 weight_map = cw_map + dw_map
 plt.figure(figsize = (20,20))
 plt.imshow(weight_map, cmap = 'jet')
