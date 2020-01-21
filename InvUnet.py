68f955f23a5b454e26541a7708d35e025cf1a1cd
zhanglf-tmac <60082189+zhanglf-tmac@users.noreply.github.com>
2020-01-21 16:36:12 +0800

    Update InvUnet.py

--- InvUnet.py
+++ InvUnet.py
@@ -3,7 +3,7 @@ from torch import nn
 
 DROPOUT_RATE = xxxx 
 
-#【0】 特征提取模块
+#【0】 Feature extraction module
 class double_conv(nn.Module):
     def __init__(self, in_ch, mid_ch, out_ch):
         super(double_conv,self).__init__()
@@ -19,7 +19,7 @@ class double_conv(nn.Module):
     def forward(self, x):
             return self.conv(x)
 
-#【1】初始卷积模块
+#【1】Input convolution module
 class in_conv(nn.Module):
     def __init__(self, in_ch, mid_ch, out_ch):
         super(in_conv,self).__init__()
@@ -28,7 +28,7 @@ class in_conv(nn.Module):
     def forward(self, x):
             return self.conv(x)      
 
-# 【2】输出卷积模块
+# 【2】Output convolution module
 class out_conv(nn.Module):
     def __init__(self, in_ch, mid_ch, out_ch, class_num):
         super(out_conv,self).__init__()
@@ -42,7 +42,7 @@ class out_conv(nn.Module):
         x = self.fc(x)
         return x
 
-# 【3】上采样模块
+# 【3】Upsampling module
 class up_sampling(nn.Module):
     def __init__(self, in_ch, out_ch, scale_factor = 2,  mode = 'bilinear'):
         super(up_sampling,self).__init__()
@@ -53,7 +53,7 @@ class up_sampling(nn.Module):
     def forward(self, x):
             return self.up(x)
 
-# 【4】Global feature 模块
+# 【4】Global feature module
 class global_feature(nn.Module):
     def __init__(self, in_size):
         super(global_feature,self).__init__()
@@ -68,24 +68,24 @@ class global_feature(nn.Module):
 from torch import nn
 from torch.nn import functional as F
 
-# 【5】搭建 InvUnet 模型
+# 【5】Build InvUnet model
 class InvUnet(nn.Module):
     def __init__(self, in_ch, out_ch,in_size, class_num):
         super(InvUnet,self).__init__()
-        self.input = in_conv(in_ch, 32,32)
-        self.conv1 = nn.Sequential(nn.MaxPool2d(2), double_conv(32, 64, 64))
-        self.conv2 = nn.Sequential(nn.MaxPool2d(2),double_conv(64, 128, 128))
-        self.conv3 = nn.Sequential(nn.MaxPool2d(2),double_conv(128, 256, 256))
-        self.conv4 = double_conv(32, 32, 32)
-        self.conv5 = double_conv(32, 32, 32)
-        self.conv6 = double_conv(32, 32, 32)
-        self.output = out_conv(32, 32, 32,class_num)
-        self.up1 = up_sampling(64, 32, scale_factor = 2)
-        self.up2 = up_sampling(128, 32, scale_factor = 4)
-        self.up3 = up_sampling(256, 32, scale_factor = 8)
-        self.gf = global_feature(in_size)
+        self.C0 = in_conv(in_ch, 32,32)
+        self.C1 = nn.Sequential(nn.MaxPool2d(2), double_conv(32, 64, 64))
+        self.C2 = nn.Sequential(nn.MaxPool2d(2),double_conv(64, 128, 128))
+        self.C3 = nn.Sequential(nn.MaxPool2d(2),double_conv(128, 256, 256))
+        self.D1 = double_conv(32, 32, 32)
+        self.D2 = double_conv(32, 32, 32)
+        self.D3 = double_conv(32, 32, 32)
+        self.D4 = out_conv(32, 32, 32,class_num)
+        self.U1 = up_sampling(64, 32, scale_factor = 2)
+        self.U2 = up_sampling(128, 32, scale_factor = 4)
+        self.U3 = up_sampling(256, 32, scale_factor = 8)
+        self.GF = global_feature(in_size)
         
-         # 参数初始化
+         #Parameter initialization
         for name,params in self.named_parameters():
             if name.find('weight') != -1 and len(params.size()) != 1:
                 nn.init.xavier_uniform(params)
@@ -93,28 +93,28 @@ class InvUnet(nn.Module):
                 nn.init.constant(params,0)
 
     def forward(self,x):
-        x1 = self.input(x)
-        x2 = self.conv1(x1)
-        x3 = self.conv2(x2)
-        x4 = self.conv3(x3)
+        x1 = self.C0(x)
+        x2 = self.C1(x1)
+        x3 = self.C2(x2)
+        x4 = self.C3(x3)
         
-        gf = self.gf(x1)
+        gf = self.GF(x1)
         x1 = x1 + gf
-        x1 = self.conv4(x1)
-        x2 = self.up1(x2)
+        x1 = self.D1(x1)
+        x2 = self.U1(x2)
         x = x1 + x2
-        x = self.conv5(x)
-        x3 = self.up2(x3)
+        x = self.D2(x)
+        x3 = self.U2(x3)
         x = x + x3
-        x = self.conv6(x)
-        x4 = self.up3(x4)
+        x = self.D3(x)
+        x4 = self.U3(x4)
         x = x + x4
-        x = self.output(x)
+        x = self.D4(x)
         return x
     
 
-#【6】针对语义分割任务的 InvUnet
+#【6】for pixel-level segmentation task,we get InvUnet
 pxl_model = InvUnet(3, 32, 256, 2)
 
-#【7】针对实例分割任务的 InvUnet-3c
+#【7】for instance-level segmentation task, we get InvUnet-3c
 ins_model = InvUnet(3, 32, 256, 3)
